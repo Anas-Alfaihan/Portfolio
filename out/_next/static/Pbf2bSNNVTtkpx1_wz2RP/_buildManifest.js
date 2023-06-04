a9f5103058e07a4c3bc8300484155e581a150024
Anas-Alfaihan <narotoaozomake99@gmail.com>
2023-06-04 18:23:18 +0300

    something

---
+++ out/_next/static/Pbf2bSNNVTtkpx1_wz2RP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-65450d44c76aad24.js","static/chunks/pages/index-f774ec7536b73949.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
